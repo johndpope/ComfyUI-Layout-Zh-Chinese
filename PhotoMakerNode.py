f4f1de99af30880a5cec96b7a1fdbed304826c92
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2024-01-16 23:45:22 +0800

    Update PhotoMakerNode.py

--- PhotoMakerNode.py
+++ PhotoMakerNode.py
@@ -1,12 +1,18 @@
+
 import torch
 import os
 import folder_paths
+from diffusers.utils import load_image
 from diffusers import EulerDiscreteScheduler
 from .pipeline import PhotoMakerStableDiffusionXLPipeline
 from huggingface_hub import hf_hub_download
-from diffusers.utils import load_image
 from .style_template import styles
+from PIL import Image
+import numpy as np
 
+# global variable
+photomaker_path = hf_hub_download(repo_id="TencentARC/PhotoMaker", filename="photomaker-v1.bin", repo_type="model")
+device = "cuda" if torch.cuda.is_available() else "cpu"
 STYLE_NAMES = list(styles.keys())
 DEFAULT_STYLE_NAME = "Photographic (Default)"
 
@@ -14,50 +20,10 @@ def apply_style(style_name: str, positive: str, negative: str = "") -> tuple[str
         p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
         return p.replace("{prompt}", positive), n + ' ' + negative
 
-class PhotoMakerNode:
-
-    @staticmethod
-    def __init__(self, base_model_path="SG161222/RealVisXL_V3.0", ref_images_path=None):
-        self.base_model_path = base_model_path
-        self.ref_images_path = ref_images_path
-        self.photomaker_path = hf_hub_download(repo_id="TencentARC/PhotoMaker", filename="photomaker-v1.bin", repo_type="model")
-        self.device = "cuda"
-        self.pipe = self.initialize_model()
-        self.input_id_images = None
-
-    def load_ref_images(self):
-        if self.ref_images_path and os.path.exists(self.ref_images_path):
-            image_basename_list = os.listdir(self.ref_images_path)
-            image_path_list = sorted([os.path.join(self.ref_images_path, basename) for basename in image_basename_list])
-
-            input_id_images = []
-            for image_path in image_path_list:
-                input_id_images.append(load_image(image_path))
-
-            return input_id_images
-        else:
-            return []
-
-    def initialize_model(self):
-        # Load base model
-        pipe = PhotoMakerStableDiffusionXLPipeline.from_pretrained(
-            self.base_model_path, 
-            torch_dtype=torch.bfloat16, 
-            use_safetensors=True, 
-            variant="fp16",
-        ).to(self.device)
-
-        # Load PhotoMaker checkpoint
-        pipe.load_photomaker_adapter(
-            os.path.dirname(self.photomaker_path),
-            subfolder="",
-            weight_name=os.path.basename(self.photomaker_path),
-            trigger_word="img"
-        )     
-        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
-        pipe.fuse_lora()
-
-        return pipe
+#batch
+class PhotoMaker_Batch_Zho:
+    def __init__(self):
+        pass
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -65,51 +31,100 @@ class PhotoMakerNode:
             "required": {
                 "base_model_path": ("STRING", {"default": "SG161222/RealVisXL_V3.0", "multiline": False}),
                 "ref_images_path": ("STRING", {"default": "./examples/newton_man"}),
-                "prompt": ("STRING", {"default": "Enter your prompt here", "multiline": True}),
-                "negative_prompt": ("STRING", {"default": "Enter negative prompt here", "multiline": True}),
+                "prompt": ("STRING", {"default": "sci-fi, closeup portrait photo of a man img wearing the sunglasses in Iron man suit, face, slim body, high quality, film grain", "multiline": True}),
+                "negative_prompt": ("STRING", {"default": "asymmetry, worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch), open mouth", "multiline": True}),
                 "style_name": (STYLE_NAMES, {"default": DEFAULT_STYLE_NAME}),
-                "style_strength_ratio": ("FLOAT", {"default": 20.0, "min": 0.0, "max": 100.0, "display": "slider"}),
-                "num_steps": ("INT", {"default": 50, "min": 1, "max": 100, "display": "slider"}),
+                "style_strength_ratio": ("INT", {"default": 20, "min": 1, "max": 50, "display": "slider"}),
+                "steps": ("INT", {"default": 50, "min": 1, "max": 100, "step": 1, "display": "slider"}),
                 "guidance_scale": ("FLOAT", {"default": 5, "min": 0.1, "max": 10.0, "step": 0.1, "display": "slider"}),
-                "seed": ("INT", {"default": 42, "min": 0})
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
             }
         }
 
     RETURN_TYPES = ("IMAGE",)
     RETURN_NAMES = ("image",)
-    FUNCTION = "generate_images"
+    FUNCTION = "process_images"
     CATEGORY = "📷PhotoMaker"
 
-    def generate_images(self, base_model_path, ref_images_path, prompt, negative_prompt, style_name, style_strength_ratio, num_steps, guidance_scale, seed):
-        self.base_model_path = base_model_path
-        self.ref_images_path = ref_images_path
-        self.pipe = self.initialize_model()  # Re-initialize the model with new path
-        self.input_id_images = self.load_ref_images()  # Reload reference images
+    def process_images(self, base_model_path, ref_images_path, prompt, negative_prompt, style_name, style_strength_ratio, steps, guidance_scale, seed):
 
-        positive, negative = self.apply_style(style_name, prompt, negative_prompt)
-        generator = torch.Generator(device=self.device).manual_seed(seed)
+        # Load base model
+        pipe = PhotoMakerStableDiffusionXLPipeline.from_pretrained(
+            base_model_path,
+            torch_dtype=torch.bfloat16,
+            use_safetensors=True,
+            variant="fp16"
+        ).to(device)
 
-        start_merge_step = int(float(style_strength_ratio) / 100 * num_steps)
+        # Load PhotoMaker checkpoint
+        pipe.load_photomaker_adapter(
+            os.path.dirname(photomaker_path),
+            subfolder="",
+            weight_name=os.path.basename(photomaker_path),
+            trigger_word="img"
+        )
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+        pipe.fuse_lora()
+        
+        # Process images
+        image_basename_list = os.listdir(ref_images_path)
+        #image_path_list = sorted([os.path.join(ref_images_path, basename) for basename in image_basename_list])
+        image_path_list = [
+            os.path.join(ref_images_path, basename) 
+            for basename in image_basename_list
+            if not basename.startswith('.') and basename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.webp'))  # 只包括有效的图像文件
+        ]
+
+        input_id_images = [load_image(image_path) for image_path in image_path_list]
+      
+        # apply the style template
+        prompt, negative_prompt = apply_style(style_name, prompt, negative_prompt)
+      
+        start_merge_step = int(float(style_strength_ratio) / 100 * steps)
         if start_merge_step > 30:
             start_merge_step = 30
 
-        images = self.pipe(
-            prompt=positive,
-            input_id_images=self.input_id_images,
-            negative_prompt=negative,
+        generator = torch.Generator(device=device).manual_seed(seed)
+
+        output = pipe(
+            prompt=prompt,
+            input_id_images=input_id_images,
+            negative_prompt=negative_prompt,
             num_images_per_prompt=1,
-            num_inference_steps=num_steps,
+            num_inference_steps=steps,
             start_merge_step=start_merge_step,
             generator=generator,
             guidance_scale=guidance_scale,
-        ).images
+            return_dict=False
+        )
 
-        return images
+        # 检查输出类型并相应处理
+        if isinstance(output, tuple):
+            # 当返回的是元组时，第一个元素是图像列表
+            images_list = output[0]
+        else:
+            # 如果返回的是 StableDiffusionXLPipelineOutput，需要从中提取图像
+            images_list = output.images
 
+        # 转换图像为 torch.Tensor
+        images_tensors = []
+        for img in images_list:
+            # 将 PIL.Image 转换为 numpy.ndarray
+            img_array = np.array(img)
+            # 转换 numpy.ndarray 为 torch.Tensor
+            img_tensor = torch.from_numpy(img_array).float() / 255.
+            # 转换图像格式为 CHW (如果需要)
+            if img_tensor.ndim == 3 and img_tensor.shape[-1] == 3:
+                img_tensor = img_tensor.permute(2, 0, 1)
+            images_tensors.append(img_tensor)
+
+        return images_tensors
+
+# Dictionary to export the node
 NODE_CLASS_MAPPINGS = {
-    "PhotoMakerNode": PhotoMakerNode
+    "PhotoMaker_Batch_Zho": PhotoMaker_Batch_Zho
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PhotoMakerNode": "Photo Maker Node"
+    "PhotoMaker_Batch_Zho": "📷PhotoMaker"
 }
